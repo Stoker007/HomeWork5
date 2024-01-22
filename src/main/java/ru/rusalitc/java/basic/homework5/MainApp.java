4aa91e5d91f89f5a04b49fc7812402ba9da8dcf9
Stoker <berdnikov.vasyliy@gmail.com>
2024-01-22 11:02:32 +0800

    Version 1

--- src/main/java/ru/rusalitc/java/basic/homework5/MainApp.java
+++ src/main/java/ru/rusalitc/java/basic/homework5/MainApp.java
@@ -1,2 +1,28 @@
-package ru.rusalitc.java.basic.homework5;public class MainApp {
+package ru.rusalitc.java.basic.homework5;
+
+import ru.rusalitc.java.basic.homework5.animals.Cat;
+import ru.rusalitc.java.basic.homework5.animals.Dog;
+import ru.rusalitc.java.basic.homework5.animals.Horse;
+
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Байкал", 10, 3, 1000);
+        Cat cat = new Cat("Мурка", 8, 0, 500);
+        Horse horse = new Horse("Ветерок", 20, 5, 2000);
+
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("У нас есть три животных: собака, кошка и лошадь");
+        System.out.println("Введите дистанцию (в метрах), чтобы понять за какое время все животные пробегут и проплывут данную дистанцию");
+        int sc = scaner.nextInt();
+        cat.run(sc);
+        dog.run(sc);
+        horse.run(sc);
+        cat.swim(sc);
+        dog.swim(sc);
+        horse.swim(sc);
+    }
 }
+
+
--- src/main/java/ru/rusalitc/java/basic/homework5/animals/Cat.java
+++ src/main/java/ru/rusalitc/java/basic/homework5/animals/Cat.java
@@ -1,8 +1,11 @@
 package ru.rusalitc.java.basic.homework5.animals;
 
-public class Dog extends Animal {
-    public Dog(String name, int speedRun, int speedSwim, int endurance){
-        super(name, speedRun, speedSwim, endurance);
-        this.dischargeSwim = 2;
+public class Cat extends Animal {
+    public Cat(String name, int speedRun, int speedSwim, int endurance) {
+        super(name, speedRun, speedSwim, endurance); // если бы в условии не было сказано, что у каждого животного есть в т.ч. и параметр: скорость плвания, можно было бы его не наследовать
+    }
+    @Override
+    public void swim (int distance) {
+        System.out.println(name + " не умеет плавать");
     }
 }
